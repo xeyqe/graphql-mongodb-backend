6bbce5a0c89b22d7087549574383ab101e916a81
xeyqe <xeyqe@seznam.cz>
2019-11-13 04:30:10 +0100

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
 const mongoose = require("mongoose");
-
+const PORT = process.env.PORT || 4000
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
@@ -10,6 +10,6 @@ mongoose.connect("mongodb+srv://new-user:MeOX1hkzsS4yHrGb@cluster0-1mlyp.mongodb
     useNewUrlParser: true
 });
 
-server.listen().then(({ url }) => {
+server.listen(PORT).then(({ url }) => {
   console.log(`Server ready at ${url}`);
 });
